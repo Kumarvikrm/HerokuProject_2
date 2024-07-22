088fb91488baba028ba90e0b2cfbcf1ca34e498a
Vikram kumar <kumarvikram68715@gmail.com>
2024-07-22 11:37:21 +0530

    Api Form

--- src/components/FormApp/index.js
+++ src/components/FormApp/index.js
@@ -1,21 +1,77 @@
-import React, { useState } from 'react'
-import './index.css';
+import {useState} from 'react';
+import swal from 'sweetalert';
 
-export default function FormApp() {
- const [set, setState] = useState("");
+const FormApp=()=>{
+  let [userName, setUsername] = useState('')
+  let [email, setEmail] = useState('')
+  let [password, setPassword] = useState('')
+  
+  
+  
+  let submitData=()=>{
+    
+    let data = {
+      "userName" : userName,
+      "email" : email,
+      "password" : password
+    }
 
- const DetailsFunc=()=>{
-  console.log(set)
- }
+    fetch("https://gorest.co.in/public-api/users", {
+      method : "POST",
+      headers : {
+        "Content-Type" : "application/json",
+        Accept : "application/json",
+        Authorization : "Bearer a9c1e9113a9be853be9d1927838b253d5904ecc78bbda55a698d5202978482e1",
+      },
+      body : JSON.stringify(data)
+    })
+     .then((response)=> {
+      console.log(response)
+      if (response.status === 200){
+        swal("Good job!", "User Registered Successfully", "success");
+      }
+      response.json()
+     })
+     .then((data)=>{
+      localStorage.setItem("userInfo", JSON.stringify(data))
+      console.log("success: ", data);
+     })
+     .catch((error) =>{
+      console.error("Error: ", Error)
+     })
 
-  return (
-    <div className='top-section'>
-      <div className='card-section'>
-        <label className='labelHeading'> Enter Value <br />
-          <input className='inputText' type="text" value={set} onChange={(e)=>{setState(e.target.value)}} placeholder='Enter Value?'/><br />
-          <input className='btn btn-outline-light' onClick={DetailsFunc} type='button' value="Save"/>
-        </label>
-      </div>    
+  }
+  
+  
+  
+  
+  
+  
+  return(
+    <div className="row">
+      <div className="col-6 mt-3 offset-3 ">
+      <h1 className="text-center"> New Form Submitted </h1>
+      <form>
+        
+        <label htmlFor="userName"></label>
+        <input type="text" id="userName" value={userName} className="form-control" 
+        placeholder="User Name" onChange={(event)=> setUsername(event.target.value)}/>
+
+        <label htmlFor="email"></label>
+        <input type="text" id="email" value={email} className="form-control" 
+        placeholder="Email" onChange={(event)=> setEmail(event.target.value)}/>
+
+        <label htmlFor="password"></label>
+        <input type="text" id="password" value={password} className="form-control" 
+        placeholder="Password" onChange={(event)=> setPassword(event.target.value)}/>
+
+        <div className="d-grid gap-2">
+        <button type="button" className="btn btn-info mt-4" onClick={()=>{submitData()}}> Save </button>
+        </div>
+      </form>
+      </div>
     </div>
   )
-};
+}
+
+export default FormApp;
